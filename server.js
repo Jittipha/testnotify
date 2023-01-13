ea4187cf246b43c9ce3d1403401675e2786d3ff2
จิตติพัฒน์ วงศ์ภูมิ <jittiphat.won@devdeva.tech>
2023-01-13 15:33:52 +0700

    ready

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
-const port =  5000;
+const port =  3000;
 const bodyParser = require('body-parser')
-const axios = require('axios')
-const qs = require('qs')
+// const axios = require('axios')
+// const qs = require('qs')
 var cors = require('cors')
 app.use(cors())
 app.use(bodyParser.json())
@@ -19,38 +19,38 @@ app.get('/api', (req, res) => {
 
 
 
-app.post('/adddata', (req, res) => {
-    let { accessCode } = req.body
-    console.log(accessCode)
-    try {
-        const url = 'https://notify-api.line.me/api/notify'
-        const jsonData = {
-            message: `สวัสดีครับ`,
-        }
-        const requestOption = {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                Authorization: `Bearer ` + accessCode,
-            },
-            data: qs.stringify(jsonData),
-            url,
-        }
-        axios(requestOption)
-            .then((axiosRes) => {
-                if (axiosRes.status === 200) {
-                    console.log('Notification Success')
-                    res.status(201).end()
-                }
-            })
-            .catch((error) => {
-                res.status(201).end()
-                console.log(error.response.data)
-            })
-    } catch (err) {
-        return res.status(400).send(err)
-    }
-})
+// app.post('/adddata', (req, res) => {
+//     let { accessCode } = req.body
+//     console.log(accessCode)
+//     try {
+//         const url = 'https://notify-api.line.me/api/notify'
+//         const jsonData = {
+//             message: `สวัสดีครับ`,
+//         }
+//         const requestOption = {
+//             method: 'POST',
+//             headers: {
+//                 'content-type': 'application/x-www-form-urlencoded',
+//                 Authorization: `Bearer ` + accessCode,
+//             },
+//             data: qs.stringify(jsonData),
+//             url,
+//         }
+//         axios(requestOption)
+//             .then((axiosRes) => {
+//                 if (axiosRes.status === 200) {
+//                     console.log('Notification Success')
+//                     res.status(201).end()
+//                 }
+//             })
+//             .catch((error) => {
+//                 res.status(201).end()
+//                 console.log(error.response.data)
+//             })
+//     } catch (err) {
+//         return res.status(400).send(err)
+//     }
+// })
 
 app.listen(port, () => {
     console.log('Node App is running or port ' + port)
